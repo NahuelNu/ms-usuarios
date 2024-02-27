34e27b67ce48b61e9c658258abd48a15b3bdbe4c
nico <dev.nicospringer@gmail.com>
2024-02-27 00:14:51 -0300

    agrego CORS

--- src/main/java/dan/ms/tp/msusuarios/rest/HealthController.java
+++ src/main/java/dan/ms/tp/msusuarios/rest/HealthController.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,13 @@ import java.net.UnknownHostException;
 @RequestMapping("api/health")
 public class HealthController {
     
+    @CrossOrigin(origins = "http://localhost:9400") // Permitir solicitudes desde localhost:9400
     @GetMapping
     public ResponseEntity<Map<String,String>> health(HttpServletRequest request){
         // Get the server name
         Map<String,String> res = new LinkedHashMap<>();
         res.put("serverName",request.getServerName());
-        res.put("app","ms-pedidos");
+        res.put("app","ms-usuarios");
         res.put("status","OK");
         res.put("timestamp",Instant.now().toString());
         try {
